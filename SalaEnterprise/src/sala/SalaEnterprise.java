ade408e50a54c857777544ab880de01236618a66
Bilal Abdulkany <bilal.abdulkany@gmail.com>
2016-01-28 15:03:43 +0530

    initial commit

---
+++ SalaEnterprise/src/sala/SalaEnterprise.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sala;
+
+/**
+ *
+ * @author bilal_a
+ */
+public class SalaEnterprise {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
